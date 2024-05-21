c2324841bb9efb0668bf4c266c0994ef7c1d192e
Thodorhs <proteopro@gmail.com>
2024-05-22 00:15:09 +0300

    Changed config.py

--- config.py
+++ config.py
@@ -1,18 +1,3 @@
-
-###################################################
-#
-# file: config.py
-#
-# @Author:   Iacovos G. Kolokasis
-# @Version:  26-01-2020
-# @email:    kolokasis@ics.forth.gr
-#
-# Note : Colors which maintain their contrast for
-# people who are colorblind.
-# (https://davidmathlogic.com/colorblind)
-#
-###################################################
-
 fullfigsize  = (13, 2)                      # Full figure size
 halffigsize  = (7, 3)                       # Half figure size
 thirdfigsize = (5, 3)                       # One third figure size
@@ -32,50 +17,3 @@ workload_name_pos = -0.05                   # Lower the position of the workload
 bbox_to_anchor = (0.5, -0.27)               # Legend position
 bar_names_loc = -22                         # Adjusted the position of the bar names
 group_spacing_factor = 0.25                 # Adjust this factor to control the spacing between groups
-
-# Color Blind pallete
-color        =   ['#3399ff',                # Blue
-                  '#ff9933',                # Orange
-                  '#00b33c',                # Green
-                  '#cc0000',                # Red
-                  '#9966ff',                # Purple
-                  '#8b7cf6',                # Light Purple
-                  '#ffc20a',                # Yellow
-                  '#7cf684',                # Light Green
-                  '#b27866',                # Brown
-                  '#66b2b1',                # Green + Blue
-                  '#d8a9d9',                # Pink
-                  '#e6cae2',                # Light Pink
-                  '#9ebbd9',                # Light Blue
-                  '#cfd8e3',                # Light Grey + Blue
-                  '#000000',                # Darg gray
-                  '#b2b2b2']
-
-# Tints of black
-monochrom   = ['#000000',                   # Black
-               '#444444',
-               '#b4b4b4',
-               '#959595',
-               '#3a3a3a',
-               '#bbbbbb',
-               '#888888',
-               '#707070',
-               '#cccccc',
-               '#e5e5e5',
-               '#ffffff',                   # White
-               '#AFAFAF']
-
-# Color blind pallete
-B_color_cycle = ['#3399ff',                 # Blue
-                 '#ff9933',                 # Orange
-                 '#00b33c',                 # Green
-                 '#cc0000',                 # Red
-                 '#9966ff',                 # Purple
-                 '#b07ce8',                 # Violet
-                 '#ff509e',                 # Magenda
-                 '#ffb000',                 # Gold
-                 '#c0bfc0']                 # Gray
-
-line_style = ['-', '--', '-.', ':', (0, (3, 1, 1, 1))]
-marker     = ['s', '^', 'o', 'D', 'X', '*', 'v', 'P']
-patterns   = ('/////', 'xxxx', '', '||||||', '-----', 'o', '.', 'O', '*', 'ooo')
